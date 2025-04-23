eb4d7ad09ba96753136b22805ebc35a6b574f041
SadheeyaM <sadheeyasalim10@gmail.com>
2025-04-23 10:31:18 +0530

    Added time zone to db file

--- database/db.php
+++ database/db.php
@@ -1,14 +1,13 @@
 <?php
-// Database connection details
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "Safgems";
 
-// Create the connection
+date_default_timezone_set('Asia/Kolkata');
+
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
